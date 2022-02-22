a426c5a26989feb30b631c78c62403e568b52c4f
Nico Van den Hooff <nicovandenhooff@gmail.com>
2022-02-21 20:54:03 -0800

    feat: update feature engineering

--- src/utils/feature_engineer.py
+++ src/utils/feature_engineer.py
@@ -6,6 +6,34 @@ def has_feature(df, feat):
     return df[feat].notna().astype(int)
 
 
+def feature_engineer(train_df, test_df):
+    """Feature engineer the wids 2022 dataset"""
+    train_df_eng = train_df.copy()
+    test_df_eng = test_df.copy()
+
+    # whether or not a building has a fog detector
+    train_df_eng["has_fog_detector"] = has_feature(train_df_eng, "days_with_fog")
+    test_df_eng["has_fog_detector"] = has_feature(test_df_eng, "days_with_fog")
+
+    # whether or not a building has a wind detector
+    train_df_eng["has_wind_detector"] = has_feature(train_df_eng, "max_wind_speed")
+    test_df_eng["has_wind_detector"] = has_feature(test_df_eng, "max_wind_speed")
+
+    return train_df_eng, test_df_eng
+
+
+def feature_engineer_multiple(train_dfs, test_dfs):
+    """Feature engineer multiple dataframes at once"""
+    train_dfs_eng = {}
+    test_dfs_eng = {}
+
+    for (name1, train_df), (name2, test_df) in zip(train_dfs.items(), test_dfs.items()):
+        assert name1 == name2
+        train_dfs_eng[name1], test_dfs_eng[name2] = feature_engineer(train_df, test_df)
+
+    return train_dfs_eng, test_dfs_eng
+
+
 def add_cluster_labels(model, ct, train_df, test_df, target):
     """Adds cluster labels as a feature"""
     train_df_cluster = train_df.copy()
